3626013e5c1a91c6f4347896e243a5a22b07bd4f
BlackGott <gott_ii@gmx.de>
2025-12-09 21:34:25 +0100

    Update two

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,32 @@ import Footer from "./components/Fotter";
 import Home from "./pages/Home";
 import Destinations from "./pages/Destinations";
 import About from "./pages/About";
+import Login from "./components/Login";
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/destinations" element={<Destinations />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-      <Footer />
+      <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+        {/* Navbar oben */}
+        <Navbar />
+
+        {/* Hauptinhalt */}
+        <main style={{ flex: 1 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/destinations" element={<Destinations />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </main>
+
+        {/* Footer unten */}
+        <Footer />
+      </div>
     </Router>
   );
 };
 
 export default App;
+
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,15 +2,38 @@ import { Link } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   return (
-    <nav style={{ padding: "1rem", background: "#0077b6", color: "white" }}>
+    <nav style={{ 
+      padding: "1rem", 
+      background: "#0077b6", 
+      color: "white", 
+      display: "flex", 
+      justifyContent: "space-between", 
+      alignItems: "center" 
+    }}>
       <h2>Travel App</h2>
-      <div>
+      <div style={{ display: "flex", alignItems: "center" }}>
         <Link to="/" style={{ margin: "0 1rem", color: "white" }}>Home</Link>
         <Link to="/destinations" style={{ margin: "0 1rem", color: "white" }}>Destinations</Link>
         <Link to="/about" style={{ margin: "0 1rem", color: "white" }}>About</Link>
+      </div>
+      {/* Login Button oben rechts */}
+      <div>
+        <Link 
+          to="/login" 
+          style={{ 
+            background: "white", 
+            color: "#0077b6", 
+            padding: "0.5rem 1rem", 
+            borderRadius: "4px", 
+            textDecoration: "none" 
+          }}
+        >
+          Login
+        </Link>
       </div>
     </nav>
   );
 };
 
 export default Navbar;
+
