cb68a73d4d56998c041a3fab1df27b0179f4f97e
Vladimir Weber <vladweb3@gmail.com>
2025-12-11 20:43:48 +0100

    update account

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import ContactForm from "./components/ContactForm/ContactForm";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <ContactForm />
     </BrowserRouter>
   </React.StrictMode>
 );
