a4aec616cd8bab2a1641af0bdf0a4b95c7f8d4a6
Yulia Kashina <Yulia.kaot@mail.ru>
2025-12-11 23:21:53 +0100

    fixed changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Destinations from "./pages/Destinations";
 import About from "./pages/About";
 import Login from "./pages/Login";
-
+import AuthTravel from "./components/AuthTravel/AuthTravel";
+import Account from "./pages/Account";
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/destinations" element={<Destinations />} />
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
+        {/* Страница регистрации / логина */}
+        <Route path="/login" element={<AuthTravel />} />
+        {/* Профиль пользователя */}
+        <Route path="/account" element={<Account />} />
       </Routes>
-    </Router>
-import { Routes, Route } from "react-router-dom";
-import AuthTravel from "./components/AuthTravel/AuthTravel";
-import Account from "./pages/Account";
-
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<AuthTravel />} />
-      <Route path="/account" element={<Account />} />
-    </Routes>
+    </>
   );
 }
-
 export default App;
-
-
-
-
--- src/main.tsx
+++ src/main.tsx
@@ -4,23 +4,14 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import "./i18n";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
-
-
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import ContactForm from "./components/ContactForm/ContactForm";
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      <ContactForm />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
