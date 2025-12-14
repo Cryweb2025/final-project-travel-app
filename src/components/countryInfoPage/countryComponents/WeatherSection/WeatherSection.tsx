df1707d09f753fa05f57ae3c6e33ce2e42cdaaf2
Ilyana Potnyaeva <il.potnyaeva@gmail.com>
2025-12-14 01:08:03 +0100

    fix css infopage,weathersection

--- src/components/countryInfoPage/countryComponents/WeatherSection/WeatherSection.tsx
+++ src/components/countryInfoPage/countryComponents/WeatherSection/WeatherSection.tsx
@@ -1,9 +1,11 @@
+
 import { useGetWeatherQuery } from "../../../../services/weatherApi";
 import styles from "./WeatherSection.module.css";
 
 type Props = {
   city: string;
 };
+
 
 function WeatherSection({ city }: Props) {
   const { data, isLoading, isError } = useGetWeatherQuery(city);
@@ -15,12 +17,19 @@ function WeatherSection({ city }: Props) {
   if (isError || !data) {
     return <div className={styles.card}>No weather data</div>;
   }
+  const description = data.weather[0].description.toLowerCase();
 
+const weatherIcon =
+  description.includes("rain") ? "🌧️" :
+  description.includes("cloud") ? "☁️" :
+  description.includes("clear") ? "☀️" :
+  description.includes("snow") ? "❄️" :
+  "🌡️";
   return (
     <div className={styles.card}>
-      <p className={styles.title}>Weather</p>
+      <p className={styles.title}> Weather</p>
       <p className={styles.text}>City: {city}</p>
-      <p className={styles.text}>
+      <p className={styles.text}> {weatherIcon}
         {Math.round(data.main.temp)}°C • {data.weather[0].description}
       </p>
     </div>
